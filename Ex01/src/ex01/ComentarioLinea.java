c26d98cdb8b69bc635eac73dbc09751714f7a99d
MarcPadrell <marc.padrell@hotmail.com>
2022-04-05 20:26:51 +0200

    File .java committed

---
+++ Ex01/src/ex01/ComentarioLinea.java
@@ -0,0 +1,9 @@
+package ex01;
+
+public class ComentarioLinea {
+	public static void main(String args[]){
+		//System.out.println("Hola");
+		System.out.println("Adios");
+		System.out.println("FIN DEL PROGRAMA");
+	}
+}
